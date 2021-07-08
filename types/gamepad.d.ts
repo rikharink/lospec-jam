3ac1546c39e8e2671fa616018e2755e86cb43456
Rik Harink <Rik.Harink@topicus.nl>
2021-07-08 19:51:39 +0200

    selection

--- types/gamepad.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Gamepad {
-    vibrationActuator?: {
-        playEffect: (type: 'dual-rumble', effect: StrictEffect) => Promise<string>;
-        reset: () => Promise<string>;
-    };
-}
